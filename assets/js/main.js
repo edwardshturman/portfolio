01d8582fea4fc392f5d8a8f11e89bc5885d9652b
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-05 01:07:56 -0700

    Add basic lighting and rotation

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,13 +14,28 @@ camera.position.setZ(30);
 renderer.render( scene, camera );
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
 
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
 function animate() {
     requestAnimationFrame( animate );
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
     renderer.render( scene, camera );
 }
 
