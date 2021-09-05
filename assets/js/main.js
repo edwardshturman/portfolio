c8d206b51fe354ad94d36c6cca91a8cf62577255
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-05 00:46:16 -0700

    Replace Three.js module with CDN

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import '../css/style.css';
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
