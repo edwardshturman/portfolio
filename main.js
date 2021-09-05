3b909cc710ce3c99cf1851f12e372ddc5f9039f1
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-05 00:17:49 -0700

    Create basic TorusGeometry

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera (75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
 });
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
 
 camera.position.setZ(30);
 
 renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus);
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+
+animate();
