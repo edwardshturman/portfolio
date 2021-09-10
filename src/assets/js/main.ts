b8db96bf6965ef9c5ec9c7d2706fcec735b1a52a
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-10 11:28:17 -0700

    Add ASCII effect

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -2,10 +2,13 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
 // @ts-ignore
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js';
+// @ts-ignore
+import { AsciiEffect } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/effects/AsciiEffect.js';
 
 
 // Setup
 
+const start = Date.now();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera ( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer({
@@ -28,6 +31,21 @@ const material = new THREE.MeshStandardMaterial( { color: 0xff6347 } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
+
+const sphere = new THREE.Mesh( new THREE.SphereGeometry( 200, 20, 10 ), new THREE.MeshPhongMaterial( { flatShading: true } ) );
+scene.add(sphere);
+
+
+const effect = new AsciiEffect( renderer, ' .:-+*=%@#', { invert: true } );
+effect.setSize( window.innerWidth, window.innerHeight );
+effect.domElement.style.color = 'white';
+effect.domElement.style.backgroundColor = 'black';
+
+// Special case: append effect.domElement, instead of renderer.domElement.
+// AsciiEffect creates a custom domElement (a div container) where the ASCII elements are placed.
+
+document.body.appendChild( effect.domElement );
+
 
 
 // Lighting
@@ -132,10 +150,32 @@ document.body.onscroll = moveCamera;
 moveCamera(); // Initialize camera movement to prevent first load glitch
 
 
+// Window resize listener
+
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    effect.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+
+
 // Animation loop
 
 function animate() {
     requestAnimationFrame( animate );
+
+    const timer = Date.now() - start;
+
+    sphere.position.y = Math.abs( Math.sin( timer * 0.002 ) ) * 150;
+    sphere.rotation.x = timer * 0.0003;
+    sphere.rotation.z = timer * 0.0002;
 
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
@@ -146,6 +186,7 @@ function animate() {
     // controls.update();
 
     renderer.render( scene, camera );
+    effect.render( scene, camera );
 }
 
 animate();
