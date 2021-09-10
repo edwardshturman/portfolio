5ef503b8573cf5c2ca887583c4fb056c7ff5e29c
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-09-10 14:47:38 -0700

    Update torus color

--- src/assets/js/main.ts
+++ src/assets/js/main.ts
@@ -24,7 +24,7 @@ renderer.render( scene, camera );
 // Add torus object
 
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshStandardMaterial( { color: 0x731b14 } );
+const material = new THREE.MeshStandardMaterial( { color: 0x99515b } );
 const torus = new THREE.Mesh( geometry, material );
 
 scene.add(torus);
